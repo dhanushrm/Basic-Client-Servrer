8ac084b31e7e14b3c44442201d30826abc9f18c2
Dhanush RM <rmdhanush@gmail.com>
2014-03-11 00:39:54 +0530

    Create Client.java

---
+++ Client.java
@@ -0,0 +1,90 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package server;
+
+/**
+ *
+ * @author IndraDhanush
+ */
+import java.awt.*;
+import java.awt.event.*;
+
+import java.io.*;
+import java.net.*;
+
+public class Client extends Frame implements ActionListener,Runnable
+{
+ //Declarations
+ Button b;
+ TextField tf;
+ TextArea ta;
+ Socket s;
+ PrintWriter pw;
+ BufferedReader br;
+ Thread th;
+ 
+ public Client()
+ {
+  Frame f=new Frame("Client");//Frame for Client
+  f.setLayout(new FlowLayout());//set layout
+  f.setBackground(Color.orange);//set background color of the Frame
+  b=new Button("Send");//Send Button
+  b.addActionListener(this);//Add action listener to send button.
+  f.addWindowListener(new W1());//add Window Listener to the Frame
+  tf=new TextField(15);
+  ta=new TextArea(50,20);
+  ta.setBackground(Color.white);
+  f.add(tf);//Add TextField to the frame
+  f.add(b);//Add send Button to the frame
+  f.add(ta);//Add TextArea to the frame
+  try{
+   s=new Socket(InetAddress.getLocalHost(),12000);//Socket for client
+   //below line reads input from InputStreamReader
+   br=new BufferedReader(new InputStreamReader(s.getInputStream()));
+   //below line writes output to OutPutStream
+   pw=new PrintWriter(s.getOutputStream(),true);
+  }catch(Exception e)  
+  {
+  }
+  th=new Thread(this);//start a new thread
+  //th.setDaemon(true);//set the thread as demon
+  th.start();
+  setFont(new Font("Courier",Font.BOLD,20));
+  f.setSize(300,300);//set the size
+  f.setVisible(true);
+  f.setLocation(100,300);//set the location
+  f.validate();
+ }
+ //method required to close the Frame on clicking "X" icon.
+ private class W1 extends WindowAdapter
+ {
+  public void windowClosing(WindowEvent we)
+  {
+   System.exit(0);
+  }
+ }
+ //This method will called after clicking on Send button.
+ public void actionPerformed(ActionEvent ae)
+ {
+  pw.println(tf.getText());//write the value of textfield into PrintWriter
+  tf.setText("");//clean the textfield
+ }
+ //Thread running as a process in background
+ public void run()
+ {
+  while(true)
+  {
+   try{
+    ta.append(br.readLine()+"\n");//Append to TextArea
+   }catch(Exception e) {}
+  }
+ }
+ //Main method
+ public static void main(String args[])
+ {
+  //Instantiate AppClient class
+  Client client = new Client();
+ }
+}
